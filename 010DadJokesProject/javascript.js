734cbccf613aa71614bec619f7d4a2a0df9dfc62
Lavon <Lavon_Edwards@hotmail.com>
2020-12-21 20:26:06 -0600

    Updated Project 010 Dad Jokes Project
    Using Async await rather than regular fetch.

--- 010DadJokesProject/javascript.js
+++ 010DadJokesProject/javascript.js
@@ -3,7 +3,7 @@ Javascript.js
 PURPOSE: Javascript for the dad jokes project.
 AUTHOR: Lavon Marquise Edwards
 DATE: December 22 2020
-NOTES: 
+NOTES: Also added sound!
 ****************************/
 
 //  The new joke button, the audio element, and the joke-text element.
@@ -26,7 +26,11 @@ button.addEventListener('click', () => {
 })
 
 //  Performs the fetch request for the joke. 
-function generateJoke() {
+//  Whenever using await for a fetch in a function, you have to make it 
+//  an async function, as seen below.
+async function generateJoke() {
+
+    // DOING IT WITH ASYNC AWAIT 
 
     //  The required header for the feth request.
     const config = {
@@ -35,14 +39,33 @@ function generateJoke() {
         }
     }
 
+    //  The asyncronous fetch request, stored in a response variable
+    const response = await fetch('https://icanhazdadjoke.com', config)
+    
+    //  the response variable, also a promise, also needs an await. Once the response json is received,
+    //  puts it into the data constant.
+    const data = await response.json();
+
+    //  Change the inner HTML to the joke.
+    joke_element.innerHTML = data.joke;
+
+
+    //THIS IS DOING IT WITH FETCH
+
+    //  The required header for the feth request.
+    // const config = {
+    //     headers: {
+    //         'Accept': 'application/json'
+    //     }
+    // }
 
     //  The actual fetch request.
-    fetch('https://icanhazdadjoke.com', config)
-    .then(response => response.json())
-    .then((data) => {
-        //  Update the inner HTML of the joke.
-        joke_element.innerHTML = data.joke
-    });
+    // fetch('https://icanhazdadjoke.com', config)
+    // .then(response => response.json())
+    // .then((data) => {
+    //     //  Update the inner HTML of the joke.
+    //     joke_element.innerHTML = data.joke
+    // });
 }
 
 
