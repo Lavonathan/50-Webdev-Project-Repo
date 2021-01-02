b3a82fb069f3d232f651ee8b1bb0d49b3139e415
Lavon <Lavon_Edwards@hotmail.com>
2021-01-01 19:22:54 -0600

    Updated project 003 Rotating Nav
    Made it so that the scroll bar is no longer shown when you rotate the page. This looks a lot better, and makes it so you can't scroll down anymore when viewing the navigation.

--- 003RotatingNavigation/javascript.js
+++ 003RotatingNavigation/javascript.js
@@ -10,12 +10,14 @@ NOTES: There is still some functionality here that I would like to add
 const open = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.container');
-const body = document.getElementsByTagName('body');
+const body = document.body;
 
 open.addEventListener('click', () => {
     container.classList.add('show-nav');
+    body.style.overflow = "hidden";
 })
 
 close.addEventListener('click',() => {
     container.classList.remove('show-nav');
+    body.style.overflow = "visible";
 })
